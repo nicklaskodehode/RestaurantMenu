a029ebb6011ffc6086f6418105f605a270e04b75
nicklaskodehode <nicklas.kodehode@gmail.com>
2025-01-14 13:59:04 +0100

    changed height on cards

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,9 +3,9 @@ import MenuCard from "./MenuCards";
 
 const CardList = (props) => {
     return (
-        <div className={styles.menuContainer}>
-            <MenuCard kategori={props.kategori} tittel={props.tittel} ingredienser={props.ingredienser} pris={props.pris}/>
-        </div>
+            <div className={styles.menuContainer}>
+                <MenuCard kategori={props.kategori} tittel={props.tittel} ingredienser={props.ingredienser} pris={props.pris}/>
+            </div>
     )
 }
 
